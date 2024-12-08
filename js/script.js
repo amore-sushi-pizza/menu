8676e67511b8d40e14981585d8680ced9c436dd4
amore-sushi-pizza <158211164+amore-sushi-pizza@users.noreply.github.com>
2024-12-08 20:20:13 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -389,16 +389,7 @@ const menu = [
         weight: "",
         img: "img/products/pizza/myslyvska.png"
     },
-    {
-        id: 329,
-        title:"Упаковка для піци",
-        Category: "pizza",
-        article: "Піца",
-        description:"",
-        price: "Діаметр 32см - 15UAH <br>Діаметр 50см - 20UAH",
-        categoryIndex: 5,
-        weight: "",
-    },
+   
     {
         id: 30,
         title:"Паляниця",
@@ -2265,7 +2256,16 @@ const menu = [
         weight: "300г",
         img: "img/products/sushi/tokio.png"
     },
-    
+    {
+        id: 329,
+        title:"Упаковка для піци",
+        Category: "pizza",
+        article: "Піца",
+        description:"",
+        price: "Діаметр 32см - 15UAH <br>Діаметр 50см - 20UAH",
+        categoryIndex: 5,
+        weight: "",
+    },
 ]
 const sections =[
     {
